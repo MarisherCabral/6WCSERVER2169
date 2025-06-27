9b3e6948b1efd84d3abe14ab879729c29a6227e1
MarisherCabral <cabralmarisher@gmail.com>
2025-06-27 13:05:19 +0800

    Add files via upload

---
+++ prelim/apps.js
@@ -0,0 +1,15 @@
+// var myLogModule = require('./utility/log.js');
+
+// myLogModule.warning('Warning node not configured..');
+// myLogModule.error('Error: Node encountered an error..');
+// myLogModule.info('Node running...');
+
+var msg = require('./utility/message.js');
+console.log(msg);
+
+// var person = require('./data.js');
+// console.log(person.firstname + '' + person.lastname);
+
+// var msg = require('./utility/log.js');
+
+// msg('hi jj!!');
---
+++ prelim/data.js
@@ -0,0 +1,4 @@
+module.exports = {
+  firstname: 'Marisher',
+  lastname: 'Cabral',
+};
