ebb296a6ca36e9fd90ee9b4b7f39512f146ac949
MarisherCabral <cabralmarisher@gmail.com>
2025-08-15 13:28:39 +0800

    Add files via upload

---
+++ express-rest/mid-post-act.js
@@ -0,0 +1,46 @@
+const express = require('express');
+const app = express();
+const PORT = 4000;
+
+app.get('/', (req, res) => {
+    res.send('Hello world!');
+});
+
+const movies = [{
+    id: 1,
+    title: 'Superman'
+},
+{
+    id: 2,
+    title: 'Thor'
+},
+{
+    id: 3,
+    title: 'Iron Man'
+}
+];
+
+app.use(express.json());
+
+app.get('/api/movies', (req, res) => {
+    res.send(movies);
+});
+
+app.get('/api/movies/:id', (req, res) => {
+    const movie = movies.find(h => h.id === parseInt(req.params.id));
+    if (!movie) return res.status(404).send('The movie with the given ID was not found.');
+    res.send(movie);
+});
+
+app.post('/api/movies', (req, res) =>{
+    const movie = {
+        id: movies.length + 1,
+        title: req.body.title
+    }
+    movies.push(movie);
+    res.send(movie);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+})
